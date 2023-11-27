8d478bdf4ec00e5166c52e0fff6e75cfa5b7b749
kavrad <kavyaht39@gmail.com>
2023-11-27 13:59:22 +0530

    Added leaderboard feature

---
+++ controllers/leaderBoardController.js
@@ -0,0 +1,20 @@
+//import users model
+const users=require('../models/users');
+
+//import expenses model
+const expenses=require('../models/expense');
+const Users = require('../models/users');
+
+//controller to fetch the leaderboard 
+exports.getLeaderBoard=async function(req,res){
+    try{
+      
+      const leaderboardUsers=await users.find({}).sort({totalExpenses:-1})
+      console.log("LeaderBoard users:",leaderboardUsers);
+   
+      return res.status(200).json(leaderboardUsers);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message:err,status:"failed"})
+    }
+}
